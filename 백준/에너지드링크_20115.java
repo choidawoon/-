cd0875c28d18d6b8f8f064372cf96d7ebe671941
choidawoon <choidawoon960717@gmail.com>
2022-02-25 22:57:13 +0900

    백준 20115

---
+++ 백준/에너지드링크_20115.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class 에너지드링크_20115 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		double arr[] = new double[N];
+		double max = 0;
+		for(int i=0; i<N; i++) {
+			arr[i] = sc.nextDouble();
+			if(max<arr[i])
+				max = arr[i];
+		}
+		
+		double sum = max;
+		for(int i=0; i<N; i++) {
+			if(arr[i]==max)
+				continue;
+			sum+=arr[i]/2;		
+		}
+		System.out.println(sum);
+		
+	}
+
+}
