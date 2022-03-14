963d121e309d90fb49d435c5ff36b4b1a79a98ed
choidawoon <choidawoon960717@gmail.com>
2022-03-14 22:46:13 +0900

    백준 9655

---
+++ 백준/돌게임_9655.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class µπ∞‘¿”_9655 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		if(N%2==0)
+			System.out.println("CY");
+		else
+			System.out.println("SK");
+		
+	}
+}
