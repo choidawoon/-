e7d0b21ac32d7bb0ccd86dd98baafb5f6fc50fe2
choidawoon <choidawoon960717@gmail.com>
2022-03-29 23:05:14 +0900

    프로그래머스 약수의 합

---
+++ 프로그래머스/약수의합.java
@@ -0,0 +1,14 @@
+class 약수의합 {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i=1; i<=n; i++) {
+        	if(n%i==0) {
+        		answer+=i;
+        		System.out.println(i);
+        	}
+        }
+        
+        return answer;
+    }
+}
