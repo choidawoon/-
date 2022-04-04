a42ae85861dfc0997647062b7a463725aacfbca1
Choi Da Woon <89010592+choidawoon@users.noreply.github.com>
2022-04-04 22:20:19 +0900

    Update 로또의최고순위와최저순위.java

--- 프로그래머스/로또의최고순위와최저순위.java
+++ 프로그래머스/로또의최고순위와최저순위.java
@@ -1,4 +1,38 @@
+import java.util.Arrays;
 
-public class ·Î¶ÇÀÇÃÖ°í¼øÀ§¿ÍÃÖÀú¼øÀ§ {
-
+public class ë¡œë˜ì˜ìµœê³ ìˆœìœ„ì™€ìµœì €ìˆœìœ„ {
+	public int[] solution(int[] lottos, int[] win_nums) {
+        int max = 0;
+        int min = 0;
+        
+        Arrays.sort(lottos);
+        Arrays.sort(win_nums);
+        
+        for(int i=0; i<6; i++) {
+        	for(int j=0; j<6; j++) {
+        		if(lottos[i]==win_nums[j]) {
+        			lottos[i] = -1;
+        			min++;
+        		}
+        	}
+        }
+        
+        max = min;
+        
+        for(int i=0; i<6; i++) {
+        	if(lottos[i]==0)
+        		max++;
+        }
+        
+        min = 7-min;
+        max = 7-max;
+        
+        if(min>=7)
+        	min = 6;
+        if(max>=7)
+        	max = 6;
+        
+        int[] answer = {max, min};
+        return answer;
+    }
 }
