3f3da6e4a8c7fd3c13f45ed322310b5e831ad8b6
ChoiDaWoon <choidawoon960717@gmail.com>
2022-04-15 18:25:48 +0900

    프로그래머스 K번째수

---
+++ 프로그래머스/K번째수.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+class K¹øÂ°¼ö {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int i=0; i<commands.length; i++) {
+        	int a = commands[i][0];
+        	int b = commands[i][1];
+        	int c = commands[i][2];
+        	int[] tmp = new int[b-a+1];
+        	int idx = 0;
+        	for(int j=a-1; j<b; j++) {
+        		tmp[idx] = array[j];
+        		idx++;
+        	}
+        	Arrays.sort(tmp);
+        	answer[i] = tmp[c-1];
+        }
+        
+        
+        
+        return answer;
+    }
+}
