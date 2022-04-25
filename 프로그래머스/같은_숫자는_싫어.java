8cb87a8cd5cad9f9e234fafd4db2cb4f9e296a0c
ChoiDaWoon <choidawoon960717@gmail.com>
2022-04-25 20:02:56 +0900

    프로그래머스 같은 숫자는 싫어

---
+++ 프로그래머스/같은_숫자는_싫어.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class 같은_숫자는_싫어 {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int tmp = 10;
+        for(int i=0; i<arr.length; i++) {
+        	if(arr[i]!=tmp) {
+        		list.add(arr[i]);
+        		tmp = arr[i];
+        	}
+        }
+        
+        answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++) {
+        	answer[i] = list.get(i);
+        }
+      
+        return answer;
+    }
+}
