c2b0905b8b962181bbfbf224f2224bbc10241fc0
ChoiDaWoon <choidawoon960717@gmail.com>
2022-04-30 13:19:52 +0900

    프로그래머스 나머지가 1이 되는수 찾기

---
+++ 프로그래머스/나머지가_1이_되는수_찾기.java
@@ -0,0 +1,15 @@
+class 나머지가_1이_되는수_찾기 {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i=2; i<n; i++) {
+        	if(n%i==1) {
+        		answer = i;
+        		break;
+        	}
+        }
+        
+        
+        return answer;
+    }
+}
