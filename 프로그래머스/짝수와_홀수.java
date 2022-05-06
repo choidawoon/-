51b9ff934e4a0fd70fbdf46cd7e49bdbe7e34c41
ChoiDaWoon <choidawoon960717@gmail.com>
2022-05-06 17:48:11 +0900

    프로그래머스 짝수와 홀수

---
+++ 프로그래머스/짝수와_홀수.java
@@ -0,0 +1,13 @@
+
+public class Â¦¼ö¿Í_È¦¼ö {
+	public String solution(int num) {
+        String answer = "";
+        if(num%2==0 || num==0){
+            answer = "Even";
+        }
+        else{
+            answer = "Odd";
+        }
+        return answer;
+    }
+}	
