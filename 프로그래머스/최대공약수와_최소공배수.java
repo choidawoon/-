7260012b18908cbe4ddcf832d56d7bcd7bf6e5de
ChoiDaWoon <choidawoon960717@gmail.com>
2022-05-23 23:46:22 +0900

    프로그래머스 최대공약수와 최소공배수

---
+++ 프로그래머스/최대공약수와_최소공배수.java
@@ -0,0 +1,16 @@
+class 최대공약수와_최소공배수 {
+  public int[] solution(int n, int m) {
+      		int[] answer = new int[2];
+		answer[0] = gcd(n, m);
+		answer[1] = n * m / answer[0];
+		return answer;
+	}
+	static int gcd(int a, int b) {
+		while(b != 0) {
+			int r = a % b;
+			a = b;
+			b = r;
+		}
+		return a;
+	}
+}
