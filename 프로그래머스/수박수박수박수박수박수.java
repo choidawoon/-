65596b0131244ea8b8e1330312b62f6bdbdec4ce
ChoiDaWoon <choidawoon960717@gmail.com>
2022-05-31 14:48:44 +0900

    프로그래머스 수박수박수박수박수박수

---
+++ 프로그래머스/수박수박수박수박수박수.java
@@ -0,0 +1,18 @@
+class 수박수박수박수박수박수 {
+    public String solution(int n) {
+        String answer = "";
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=1; i<=n; i++) {
+        	if(i%2!=0)
+        		sb.append("수");
+        	else
+        		sb.append("박");
+        }
+        
+        answer = sb.toString();
+        
+        return answer;
+    }
+}
