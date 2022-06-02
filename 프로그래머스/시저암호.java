61547a728ef209d0840a9e66867735d6099578d8
ChoiDaWoon <choidawoon960717@gmail.com>
2022-06-02 23:47:24 +0900

    프로그래머스 시저암호

---
+++ 프로그래머스/시저암호.java
@@ -0,0 +1,37 @@
+class 시저암호 {
+    public String solution(String s, int n) {
+        String answer = "";
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0; i<s.length(); i++) {
+        	
+        	char tmp = s.charAt(i);
+        	
+        	if(tmp==' ') {
+        		sb.append(tmp);
+        	}
+        	
+        	if(tmp>='a' && tmp<='z') {
+        		if(tmp+n>'z') 
+        			tmp = (char)(tmp+n-26);
+        		else
+        			tmp = (char)(tmp+n);
+        		sb.append(tmp);
+        	}
+        	
+        	if(tmp>='A' && tmp<='Z') {
+        		if(tmp+n>'Z') 
+        			tmp = (char)(tmp+n-26);
+        		else
+        			tmp = (char)(tmp+n);
+        		sb.append(tmp);
+        	}
+        	
+        }
+        
+        answer = sb.toString();
+        return answer;
+        
+    }
+}
