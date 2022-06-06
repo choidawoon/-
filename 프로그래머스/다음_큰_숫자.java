d56b35f71dabf7b010842b1a6ee814495208b7f7
ChoiDaWoon <choidawoon960717@gmail.com>
2022-06-06 15:12:32 +0900

    프로그래머스 다음 큰 숫자

---
+++ 프로그래머스/다음_큰_숫자.java
@@ -0,0 +1,34 @@
+class 다음_큰_숫자 {
+    public int solution(int n) {
+        int answer = 0;
+        
+        String str = Integer.toBinaryString(n);
+        int cnt = 0;
+        
+        for(int i=0; i<str.length(); i++) {
+        	if(str.charAt(i)=='1')
+        		cnt++;        	
+        }
+        
+        	for(int i=n+1; i<1000000; i++) {
+        		String tmp = Integer.toBinaryString(i);
+        		int tmp_cnt = 0;
+        		
+        		for(int j=0; j<tmp.length(); j++) {
+        			if(tmp.charAt(j)=='1')
+        				tmp_cnt++;
+        			if(tmp_cnt>cnt)
+        				break;
+        		}
+        		
+        		if(tmp_cnt==cnt) {
+        			answer = i;
+        			break;
+        		}
+        		
+        	}
+        
+        return answer;
+        
+    }
+}
