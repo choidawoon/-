638dbad8fe146d07681970c4631b5dd1b58ad794
ChoiDaWoon <choidawoon960717@gmail.com>
2022-06-06 15:11:57 +0900

    프로그래머스 숫자의표현

---
+++ 프로그래머스/숫자의표현.java
@@ -0,0 +1,24 @@
+class 숫자의표현 {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i=1; i<=n; i++) {
+        	
+        	int sum  = 0;
+        	int idx = i;
+        	
+        	while(sum+idx<=n) {
+        		sum += idx;
+        		idx++;
+        	}//while
+        	
+        	
+        	if(sum==n)
+        		answer++;
+ 
+        }//for
+        
+        return answer;
+        
+    }
+}
