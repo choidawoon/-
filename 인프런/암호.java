0c17e6ad5bf85f22ace67a595d8ee2ab1879ad6e
ChoiDaWoon <choidawoon960717@gmail.com>
2022-07-07 15:25:10 +0900

    인프런 암포

--- 인프런/암호.java
+++ 인프런/암호.java
@@ -13,23 +13,32 @@ public class 암호 {
 		int idx = 7;
 		StringBuilder result = new StringBuilder();
 		
+//		for(int i=0; i<N; i++) {
+//			
+//			String tmp = str.substring(i*7,idx);
+//			StringBuilder sb = new StringBuilder();
+//			
+//			for(int j=0; j<7; j++) {
+//				if(tmp.charAt(j)=='#')
+//					sb.append("1");
+//				else
+//					sb.append("0");
+//			}
+//			
+//			int askii = Integer.parseInt(sb.toString(), 2);
+//			char ch = (char)askii;
+//			result.append(ch);
+//			idx =  idx + 7;
+//			
+//		}
+		
 		for(int i=0; i<N; i++) {
+			String tmp = str.substring(0,7).replace('#', '1').replace('*', '0');
+			int askii = Integer.parseInt(tmp,2);
 			
-			String tmp = str.substring(i*7,idx);
-			StringBuilder sb = new StringBuilder();
-			
-			for(int j=0; j<7; j++) {
-				if(tmp.charAt(j)=='#')
-					sb.append("1");
-				else
-					sb.append("0");
-			}
-			
-			int askii = Integer.parseInt(sb.toString(), 2);
 			char ch = (char)askii;
 			result.append(ch);
-			idx =  idx + 7;
-			
+			str = str.substring(7);
 		}
 		
 		System.out.println(result.toString());
