69b0e2586b3bc32c714627c94fdaef6fc98f0207
ChoiDaWoon <choidawoon960717@gmail.com>
2022-07-18 23:25:23 +0900

    인프런 교육과정 설계

---
+++ 인프런/교육과정_설계.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class 교육과정_설계 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		String sub = sc.next();
+		String str = sc.next();
+		
+		Queue<Character> qu = new LinkedList<Character>();
+		for(int i=0; i<sub.length(); i++) {
+			qu.add(sub.charAt(i));
+		}
+		
+		for(int i=0; i<str.length(); i++) {
+			if(str.charAt(i)==qu.peek())
+				qu.poll();
+			if(qu.isEmpty())
+				break;
+		}
+		
+		if(qu.isEmpty())
+			System.out.println("YES");
+		else
+			System.out.println("NO");
+		
+	}
+}
