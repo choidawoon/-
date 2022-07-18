336d7b205d7b3a04377240d3eabf4d3d2af92e9e
Choi Da Woon <89010592+choidawoon@users.noreply.github.com>
2022-07-19 00:14:48 +0900

    인프런 응급실

--- 인프런/응급실.java
+++ 인프런/응급실.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -6,28 +7,52 @@ public class 응급실 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		Scanner sc = new Scanner(System.in);
+	
+		int N = sc.nextInt();
+		int M = sc.nextInt();
 		
-		String sub = sc.next();
-		String str = sc.next();
+		Queue<person> qu = new LinkedList<person>();
+		int arr[] = new int[N];
+		for(int i=0; i<N; i++)
+			arr[i] = sc.nextInt();
 		
-		Queue<Character> qu = new LinkedList<Character>();
-		for(int i=0; i<sub.length(); i++) {
-			qu.add(sub.charAt(i));
-		}
+		int result = 1;
 		
-		for(int i=0; i<str.length(); i++) {
-			if(str.charAt(i)==qu.peek())
-				qu.poll();
-			if(qu.isEmpty())
-				break;
-		}
+		for(int i=0; i<N; i++)
+			qu.add(new person(i, arr[i]));			
 		
-		if(qu.isEmpty())
-			System.out.println("YES");
-		else
-			System.out.println("NO");
+		while(!qu.isEmpty()) {
+			
+			person tmp = qu.poll();
+			
+			for(person x : qu) {
+				if(x.priority>tmp.priority) {
+					qu.add(tmp);
+					tmp = null;
+					break;
+				}
+			}
+			
+			if(tmp!=null) {
+				if(tmp.id==M)
+					break;
+				else
+					result++;
+			}
+			
+		}//while
 		
+		System.out.println(result);
+		
+	}
+}
+
+class person{
+	int id;
+	int priority;
+	public person(int id, int priority) {
+		this.id = id;
+		this.priority = priority;
 	}
 }
